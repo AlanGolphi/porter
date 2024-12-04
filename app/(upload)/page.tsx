039bf90c472fe0c564a8eb442323387ee66f3627
AlanGolphi <alangolphi@163.com>
2024-12-04 09:02:00 +0000

    move components to right folder

--- app/(upload)/page.tsx
+++ app/(upload)/page.tsx
@@ -1,7 +1,7 @@
 import { Skeleton } from '@/components/ui/skeleton'
 import dynamic from 'next/dynamic'
 
-const UploadSection = dynamic(() => import('../components/upload-section'), {
+const UploadSection = dynamic(() => import('./components/upload-section'), {
   loading: () => <Skeleton className="h-24 w-full rounded-2xl bg-zinc-50" />,
 })
 
