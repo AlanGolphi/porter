9bc9142f7e70ef910fac99bd9ae3acb9d4e8d2a3
AlanGolphi <alangolphi@163.com>
2024-12-17 07:35:29 +0000

    feat: add mobile nav

--- app/(upload)/layout.tsx
+++ app/(upload)/layout.tsx
@@ -6,7 +6,7 @@ export default function UploadLayout({
   children: React.ReactNode
 }>) {
   return (
-    <main className="relative flex min-h-dvh w-full bg-backgroundMud p-4 md:pl-28">
+    <main className="relative flex min-h-dvh w-full min-w-80 bg-backgroundMud p-4 pt-24 md:pl-28 md:pt-4">
       <GlobalNav />
       {children}
     </main>
--- app/components/global-nav.tsx
+++ app/components/global-nav.tsx
@@ -1,31 +1,56 @@
 import Logo from '@/app/assets/images/logo.png'
-import { CircleUser, Moon, Settings, Sun } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { CircleUser, Menu, Moon, Settings, Sun } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function GlobalNav() {
   return (
-    <header className="fixed left-4 top-4 z-50 hidden h-[calc(100dvh-2rem)] w-20 rounded-2xl bg-card-mud p-4 md:flex">
-      <div className="flex h-full w-full flex-col items-center justify-between">
-        <Link href="/" className="flex flex-col items-center space-y-2">
-          <Image src={Logo} alt="Logo" width={48} height={48} className="h-12 w-12" />
-          <span className="text-lg font-bold">porter</span>
-        </Link>
-        <nav>
-          <ul className="flex flex-col items-center justify-end space-y-4">
-            <li className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-white dark:bg-slate-950">
-              <Moon className="hidden h-6 w-6 dark:block" />
-              <Sun className="block h-6 w-6 dark:hidden" />
-            </li>
-            <li className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-white dark:bg-slate-950">
-              <Settings className="h-6 w-6" />
-            </li>
-            <li className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-white dark:bg-slate-950">
-              <CircleUser className="h-6 w-6" />
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+    <>
+      {/* mobile navbar */}
+      <header className="fixed left-4 top-4 z-50 w-[calc(100%-2rem)] min-w-72 rounded-2xl bg-card-mud px-4 py-2 md:hidden">
+        <div className="flex w-full items-center justify-between">
+          <Link href="/" className="flex items-center space-x-2">
+            <Image src={Logo} alt="Logo" width={48} height={48} className="h-12 w-12" />
+            <span className="text-lg font-bold">porter</span>
+          </Link>
+          <Menu className="!size-6" />
+        </div>
+      </header>
+
+      {/* desktop sidebar */}
+      <header className="fixed left-4 top-4 z-50 hidden h-[calc(100dvh-2rem)] w-20 rounded-2xl bg-card-mud p-4 md:flex">
+        <div className="flex h-full w-full flex-col items-center justify-between">
+          <Link href="/" className="flex flex-col items-center space-y-2">
+            <Image src={Logo} alt="Logo" width={48} height={48} className="h-12 w-12" />
+            <span className="text-lg font-bold">porter</span>
+          </Link>
+          <nav className="flex flex-col items-center justify-end space-y-4">
+            <Button
+              size="icon"
+              variant="outline"
+              className="flex h-12 w-12 items-center justify-center rounded-full bg-white dark:bg-slate-950"
+            >
+              <Moon className="hidden !size-6 dark:block" />
+              <Sun className="block !size-6 dark:hidden" />
+            </Button>
+            <Button
+              size="icon"
+              variant="outline"
+              className="flex h-12 w-12 items-center justify-center rounded-full bg-white dark:bg-slate-950"
+            >
+              <Settings className="!size-6" />
+            </Button>
+            <Button
+              size="icon"
+              variant="outline"
+              className="flex h-12 w-12 items-center justify-center rounded-full bg-white dark:bg-slate-950"
+            >
+              <CircleUser className="!size-6" />
+            </Button>
+          </nav>
+        </div>
+      </header>
+    </>
   )
 }
