b37c35023841adafdf356d9ece8e7efad5a57867
AlanGolphi <alangolphi@163.com>
2025-02-17 03:15:50 +0000

    feat: add tooltip for copyable-file-url compo

--- app/(upload)/components/copyable-file-url.tsx
+++ app/(upload)/components/copyable-file-url.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 import { truncateFilename } from '@/lib/utils'
+import { Copy } from 'lucide-react'
 import { useTranslations } from 'next-intl'
 import { useCallback } from 'react'
 import { toast } from 'sonner'
@@ -51,22 +53,32 @@ export function CopyableFileUrl({ url, fileSize, maxLength = 20 }: CopyableFileU
   if (!renderUrl) return null
 
   return (
-    <div
-      className="flex items-center justify-center text-center"
-      title={`size: ${formatFileSize(fileSize)}`}
-    >
-      <a
-        href={url}
-        target="_blank"
-        rel="noopener noreferrer"
-        onClick={(e) => {
-          e.preventDefault()
-          clickToCopy(url)
-        }}
-        className="text-nowrap text-blue-800 hover:text-blue-900 dark:text-blue-300 dark:hover:text-blue-500"
-      >
-        {truncateFilename(renderUrl, maxLength)}
-      </a>
-    </div>
+    <TooltipProvider>
+      <Tooltip delayDuration={300}>
+        <TooltipTrigger>
+          <div
+            className="flex items-center justify-center text-center"
+            title={`size: ${formatFileSize(fileSize)}`}
+          >
+            <a
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={(e) => {
+                e.preventDefault()
+                clickToCopy(url)
+              }}
+              className="gap-1 text-nowrap text-blue-800 hover:text-blue-900 dark:text-blue-300 dark:hover:text-blue-500"
+            >
+              {truncateFilename(renderUrl, maxLength)}
+            </a>
+          </div>
+        </TooltipTrigger>
+        <TooltipContent className="flex items-center gap-1">
+          <Copy className="h-4 w-4 text-white dark:text-black" />
+          <p>{t('ClickToCopyUrl')}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
