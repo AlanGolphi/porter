786c6d3295898c5ab8f85d3f38abde6cf9faab7c
AlanGolphi <alangolphi@163.com>
2025-02-17 07:13:15 +0000

    feat: remove console in test and prod, exclude error

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts')
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development' ? { exclude: ['error'] } : false,
+  },
 }
 
 export default withNextIntl(nextConfig)
